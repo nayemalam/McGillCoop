e0e9cb531f78daf45016e78eb5631f92d65c78e3
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-02-09 13:54:45 -0500

    formatting

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/model/User.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/model/User.java
@@ -5,30 +5,37 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class User{
+   
    private String name;
 
-public void setName(String value) {
-    this.name = value;
-}
-public String getName() {
-    return this.name;
-}
-private String username;
+   public void setName(String value) {
+       this.name = value;
+   }
+   
+   public String getName() {
+       return this.name;
+   }
+   
+   private String username;
 
-public void setUsername(String value) {
-    this.username = value;
-}
-public String getUsername() {
-    return this.username;
-}
-private String password;
+   public void setUsername(String value) {
+       this.username = value;
+   }
+   
+   public String getUsername() {
+       return this.username;
+   }
+   
+   private String password;
 
-public void setPassword(String value) {
-    this.password = value;
-}
-public String getPassword() {
-    return this.password;
-}
+   public void setPassword(String value) {
+       this.password = value;
+   }
+   
+   public String getPassword() {
+       return this.password;
+   }
+   
    private CoOperatorSystem coOperatorSystem;
    
    @ManyToOne(optional=false)
@@ -40,4 +47,4 @@ public String getPassword() {
       this.coOperatorSystem = coOperatorSystem;
    }
    
-   }
+}
