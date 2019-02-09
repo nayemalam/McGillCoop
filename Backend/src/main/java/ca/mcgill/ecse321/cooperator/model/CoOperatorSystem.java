4c1520c1f961c0defe2de21ac2bf99265dc1fdec
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-02-09 13:52:02 -0500

    Slight changes

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/model/CoOperatorSystem.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/model/CoOperatorSystem.java
@@ -1,7 +1,7 @@
 package ca.mcgill.ecse321.cooperator.model;
 
-import javax.persistence.Entity;
 import java.util.Set;
+import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.CascadeType;
 
@@ -40,4 +40,4 @@ public class CoOperatorSystem{
       this.user = users;
    }
    
-   }
+}
