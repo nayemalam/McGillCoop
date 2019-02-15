866d35bd264cfe0071c88e0ee29b0950c3df534d
Tristan Bouchard <tbouchard1997@gmail.com>
2019-02-15 16:32:44 -0500

    Create UpdateStudent Transaction method to update Student in database
    Related to issue #28

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/model/Student.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/model/Student.java
@@ -1,4 +1,5 @@
 package ca.mcgill.ecse321.cooperator.model;
+
 import javax.persistence.ManyToMany;
 
 import javax.persistence.Entity;
@@ -7,34 +8,36 @@ import java.util.Set;
 import javax.persistence.OneToMany;
 
 @Entity
-public class Student extends SystemUser{
-private Set<CoopTerm> coopTerm;
-   
-   @OneToMany(mappedBy="student" )
+public class Student extends SystemUser {
+   private Set<CoopTerm> coopTerm;
+
+   @OneToMany(mappedBy = "student")
    public Set<CoopTerm> getCoopTerm() {
       return this.coopTerm;
    }
-   
+
    public void setCoopTerm(Set<CoopTerm> coopTerms) {
       this.coopTerm = coopTerms;
    }
-   
+
    private String program;
 
-public void setProgram(String value) {
-this.program = value;
-}
-public String getProgram() {
-return this.program;
-}
-
-private Integer studentId;
-
-public void setStudentId(Integer value) {
-this.studentId = value;
-}
-public Integer getStudentId() {
-return this.studentId;
-}
-   
+   public void setProgram(String value) {
+      this.program = value;
    }
+
+   public String getProgram() {
+      return this.program;
+   }
+
+   private Integer studentId;
+
+   public void setStudentId(Integer value) {
+      this.studentId = value;
+   }
+
+   public Integer getStudentId() {
+      return this.studentId;
+   }
+
+}
