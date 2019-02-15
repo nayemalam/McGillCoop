cec9ced0af4017c81fb2706728332000ad9d0586
amasciotra <alexmasciotra@gmail.com>
2019-02-15 14:29:54 -0500

    Create dao classes for each class
    related to issue #34

---
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/dao/CoopAdministratorRepository.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.cooperator.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import ca.mcgill.ecse321.cooperator.model.CoopAdministrator;
+
+
+
+public interface CoopAdministratorRepository extends CrudRepository<CoopAdministrator, Integer> {
+
+	CoopAdministrator findByuserID(Integer id);
+
+}
--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/dao/CoopTermRepository.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/dao/CoopTermRepository.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse321.cooperator.dao;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.cooperator.model.CoopTerm;
 
 public interface CoopTermRepository extends CrudRepository<CoopTerm, Integer> {
 	
-	CoopTerm findBytermId(Integer id);
+	CoopTerm findBytermId(Integer termId);
+	List<CoopTerm> findByStartDate(Date startDate);
 
 }
