6ce08b0c1654bdc842fd3c1aed5907dca3bc104a
amasciotra <alexmasciotra@gmail.com>
2019-03-01 17:49:05 -0500

    create controller class WIP
    related to issue #68

---
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/CooperatorController.java
@@ -0,0 +1,26 @@
+package ca.mcgill.ecse321.cooperator.controller;
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@CrossOrigin(origins = "*")
+@RestController
+public class CooperatorController {
+	
+	
+
+}
+
