bf4194ea84104d82dddeaff55f7de3e4b820ea9e
Thomas Philippon <thomas.philippon@mail.mcgill.ca>
2019-03-21 22:59:30 -0400

    Frontend implementation of the get student business method
    related to issue #112

--- Frontend/src/components/coopadmin.js
+++ Frontend/src/components/coopadmin.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 var config = require('../../config')
 
-// Uncomment below for LOCAL test
+//Uncomment below for LOCAL test
 
   var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
   var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
-// Uncomment below for LIVE server test
-    // var frontendUrl = 'https://' + config.build.host
-    // var backendUrl = 'https://' + config.build.backendHost 
+// //Uncomment below for LIVE server test
+//     var frontendUrl = 'https://' + config.build.host
+//     var backendUrl = 'https://' + config.build.backendHost 
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
