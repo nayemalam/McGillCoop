c97eed2898319e7d095a540366f5f6e094af0a46
Thomas Philippon <thomas.philippon@mail.mcgill.ca>
2019-03-23 18:38:44 -0400

    Frontend list students and list employer
    related to issue #112
    
    Please enter the commit message for your changes. Lines starting

--- Frontend/src/components/Cooperator.vue
+++ Frontend/src/components/Cooperator.vue
@@ -1,87 +1,95 @@
+<!--
 <template>
   <div id="coopAdmins" class="container">
     <h2>Administrators Portal</h2>
-    <div class="row">
-      <div class="col-xs-6">
-        <table>
-          <tr>
-            <td>
-              <form><h4>Add Administrator</h4> <br/>
-                <div class="field">
-                  <div class="control">
-                    <input type="text" v-model="newAdmin.firstName" placeholder="First Name"> <br/><br/>
-                    <input type="text" v-model="newAdmin.lastName" placeholder="Last Name"> <br/><br/>
-                    <input type="text" v-model="newAdmin.emailAddress" placeholder="Email Address"> <br/><br/>
-                    <input type="text" v-model="newAdmin.userName" placeholder="userName"> <br/><br/>
-                    <input type="text" v-model="newAdmin.password" placeholder="Password"> <br/>           
-                  </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <table>
+            <tr>
+              <td>
+                <form><h4>Add Administrator</h4> <br/>
+                  <div class="field">
+                    <div class="control">
+                      <input type="text" v-model="newAdmin.firstName" placeholder="First Name"> <br/><br/>
+                      <input type="text" v-model="newAdmin.lastName" placeholder="Last Name"> <br/><br/>
+                      <input type="text" v-model="newAdmin.emailAddress" placeholder="Email Address"> <br/><br/>
+                      <input type="text" v-model="newAdmin.userName" placeholder="userName"> <br/><br/>
+                      <input type="text" v-model="newAdmin.password" placeholder="Password"> <br/>           
+                    </div>
                     <br/>
                     <div class="form">
                       <button type="submit" class="btn btn-primary" @click="createAdmin(newAdmin)">
                         Create Administrator
                       </button>
                     </div>
-                </div>
-              </form>  
-            </td>
-          </tr>
-        </table>
-      </div>
-      <!-- PREVIEW INPUT -->
-      <div class="col-xs-6">
-      <br/><br/>
-      <h4>Previewed Input</h4>
-      <table>
-        <tr v-for="admin in coopAdmins" >
-          <td> Email: {{ admin.emailAddress }},</td>
-          <td> UserName: {{ admin.userName }},</td>
-          <td> FirstName: {{ admin.firstName }},</td> 
-          <td> LastName: {{ admin.lastName }}</td>
-          <td>{{ admin.password }}</td>
-        </tr>
-      </table>
-      </div>
+                  </div>
+                </form>  
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div class="col-sm-4">
+          <br/><br/>
+          <h4>Previewed Input</h4>
+          <table>
+            <tr v-for="admin in coopAdmins" >
+              <td> Email: {{ admin.emailAddress }},</td>
+              <td> UserName: {{ admin.userName }},</td>
+              <td> FirstName: {{ admin.firstName }},</td> 
+              <td> LastName: {{ admin.lastName }}</td>
+              <td>{{ admin.password }}</td>
+            </tr>
+          </table>
+        </div>
       </div>
       <p>
-      <span v-if="errorAdmin" style="color:red">Error: {{errorAdmin}} </span>
-    </p>
-  </div>
+      
+        <span v-if="errorAdmin" style="color:red">Error: {{errorAdmin}} </span>
+      </p>
+      -->
+<template>
+  <div id="coopadmins" class="container">
+    <div class="row">
+      <div class="col-sm-8">
+        <br/><br/>
+          <table>
+            <tr>
+              <h4>List Employer </h4>
+                <button type="submit" class="btn btn-primary" @click="getStudentTerm()">
+                List Employer
+                </button>
+            </tr>
+          </table>
+      </div>
+    </div>
+    <div class="col-sm-8">
+      <br/><br/>
+      <h4>Previewed Input</h4>
+        <table>
+          <tr v-for="student in students" >
+          <td> Email: {{ student.emailAddress }},</td>
+          <td> UserName: {{ student.userName }},</td>
+          <td> FirstName: {{ student.firstName }},</td> 
+          <td> LastName: {{ student.lastName }}</td>
+          <td> Program: {{ student.location}}</td>
+        </tr>
+      </table>
+    </div>
+  </div> 
 </template>
 
 <script src="./coopadmin.js">
 </script>
 
-<template>
-    <div id = "students" class = "container">
-      <div class="col-xs-6">
-        <br/><br/>
-          <table>
-            <tr>
-              <h2>List Students </h2>
-                <button type="submit" class="btn btn-primary" @click="getStudents()">
-                List Students
-                </button>
-            </tr>
-        </table>
-      </div>
-      <!-- PREVIEW INPUT -->
-      <div class="col-xs-6">
-      <br/><br/>
-      <h4>Previewed Input</h4>
-      <table>
-        <tr v-for="student in students" >
-          <td> Email: {{ student.emailAddress }},</td>
-          <td> UserName: {{ student.userName }},</td>
-          <td> FirstName: {{ student.firstName }},</td> 
-          <td> LastName: {{ student.lastName }}</td>
-        </tr>
-      </table>
-      </div>
-  </div>
-</template>
-
-<script src="./student.js">
+<!--
+<script src="./coopadmin.js">
 </script>
+-->
+
+
+
+
+
 
 <style>
   #eventregistration {
--- Frontend/src/components/student.js
+++ Frontend/src/components/student.js
@@ -14,47 +14,68 @@ var AXIOS = axios.create({
   baseURL: backendUrl,
   headers: { 'Access-Control-Allow-Origin': frontendUrl }
  })
-export default {
-  name: 'students',
-  data () {
-    return {
-      students: [],
-      newStudent: {
-        lastName: '', //lastName
-        firstName: '', //firstName
-        emailAddress: '',
-        userName: '',
-      },
-      errorStudent: '',
-      response: [],
-    }
-  },
-  created: function () {
-    // Initializing admins from backend
-      AXIOS.get(`/students`)
-      .then(response => {
-        console.log(response.data)
-        this.students = response.data
-      })
-      .catch(e => {
-        this.errorStudent = e;
-      });
-  },
-  methods: {
-    getStudents: function () {
-      AXIOS.get('/students')
-      .then(response => {
-        // JSON responses are automatically parsed.
-        this.students = response.data
-        this.errorStudent = ''
-      })
-      .catch(e => {
-        var errorMsg = e.message
-        console.log(errorMsg)
-        this.errorStudent = errorMsg
-      });
-      console.log("im in here!!!")
-    }
-  }
-}
+
+
+
+// export default {
+//   name: 'students',
+//   data () {
+//     return {
+//       students: [],
+//       newStudent: {
+//         lastName: '', //lastName
+//         firstName: '', //firstName
+//         emailAddress: '',
+//         userName: '',
+//         program:'',
+//         coopTerm: [],
+//       },
+//       errorStudent: '',
+//       response: [],
+
+//     }
+
+
+//   },
+//   // name: 'coopTerms',
+//   // data () {
+//   //   return {
+//   //     coopTerms: [],
+//   //     newCoopTerm: {
+//   //       termId:'',
+//   //       startDate: '',
+//   //       endDate:'',
+//   //       document: [],
+//   //     },
+//   //     errorCoopTerm: '',
+//   //     response: [],
+//   //   }
+//   // },
+//   created: function () {
+//   },
+//   methods: {
+//     getStudents: function () {
+//       AXIOS.get('/students')
+//       .then(response => {
+//         // JSON responses are automatically parsed.
+//         this.students = response.data
+//         this.errorStudent = ''
+//       })
+//       .catch(e => {
+//         var errorMsg = e.message
+//         console.log(errorMsg)
+//         this.errorStudent = errorMsg
+//       });
+//       console.log("im in here!!!")
+//     },
+//     getStudentTerm: function(){
+      
+//       console.log("HEllOO")
+      
+
+  
+
+//     }
+//   }
+// }
 console.log("test student")
