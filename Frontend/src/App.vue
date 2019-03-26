6f5f9a2324d60a678710d4d044997745f7ff42bb
name <nayem.alam@mail.mcgill.ca>
2019-03-25 20:01:22 -0400

    added https

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -18,7 +18,7 @@
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
             <b-nav-item><router-link to="/coopadmin" style="color:white; decoration: none;" ><b-button size="md" class="my-2 my-sm-0"><img src="https://img.pngio.com/white-people-png-svg-royalty-free-white-person-png-1000_758.png" width="21"/>  Sign up</b-button></router-link></b-nav-item>
-            <b-nav-item><router-link to="/" style="color:white; decoration: none;" ><b-button size="md" class="my-2 my-sm-0" variant="danger" style="color: white;"><img src="http://softcom2019.fesb.unist.hr/wp-content/uploads/2016/07/register-icon.png" width="21"/>  Log in</b-button></router-link></b-nav-item>
+            <b-nav-item><router-link to="/" style="color:white; decoration: none;" ><b-button size="md" class="my-2 my-sm-0" variant="danger" style="color: white;"><img src="https://res.cloudinary.com/nayemalam/image/upload/v1553558327/ECSE321/login.png" width="21"/>  Log in</b-button></router-link></b-nav-item>
 
             <!-- <b-nav-form>
               <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search" />
