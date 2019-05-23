6d231f9443747dcc9370724124c9c5cd1b976c78
Nayem Alam <nayem.alam@mail.mcgill.ca>
2019-05-23 22:49:22 +0800

    added RequestMapping to let swagger-ui point to this file

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/CooperatorController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/CooperatorController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -52,6 +53,7 @@ import ca.mcgill.ecse321.cooperator.service.CooperatorService;
  */
 @CrossOrigin
 @RestController
+@RequestMapping("/CooperatorController")
 public class CooperatorController {
 
 	@Autowired
