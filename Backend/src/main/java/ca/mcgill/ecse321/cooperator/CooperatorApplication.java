c06fba4f3d717b13348bd80c13815b681893f6fd
Nayem Alam <nayem.alam@mail.mcgill.ca>
2019-05-23 22:48:19 +0800

    incorporate swagger-ui dependency

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/CooperatorApplication.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/CooperatorApplication.java
@@ -9,12 +9,19 @@ import javax.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.PathSelectors;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @SpringBootApplication
+@EnableSwagger2
 public class CooperatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CooperatorApplication.class,args);
@@ -22,7 +29,16 @@ public class CooperatorApplication {
 
   @RequestMapping("/")
   public String greeting(){
-    return "hello";
+    return "Hello testing";
   }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/CooperatorController.*"))
+                .build();
+    }
   
 }
